41db2a15e61455d46aaba9c6f292a961566451fd
Ashish Patel <ashishpatel0720@gmail.com>
2020-08-31 11:06:09 +0530

    Update TheFooter.vue

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -4,12 +4,12 @@
     <div class="columns is-multiline is-mobile">
       <div class="column is-6-desktop is-6-tablet is-6-widescreen is-12-mobile ">
         <p class="has-text-centered-mobile">
-       Made with <i class="fas fa-heart"></i>
+       Made with <i class="fas fa-heart"></i> by <a href="https://ashish.live" target="_blank">Ashish Patel</a>
       </p>
       </div>
       <div class="column is-6-desktop is-6-tablet is-6-widescreen is-12-mobile">
         <p class="has-text-right-desktop has-text-right-widescreen  has-text-right-tablet has-text-centered-mobile">
-       Powered by: Samiksha and her will :)
+       Powered by: VueJs and Unsplash
       </p>
       </div>
       </div>
